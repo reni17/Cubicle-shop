a8b46a3a477fd6b72a79277fa6ab9cf4f0a0792e
reni17 <renit00@mail.bg>
2022-05-28 23:49:09 +0300

    Render home page and add static files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const hadlebars = require('express-handlebars');
 const app = express()
 
+app.use('/static', express.static('./src/public'));
+
+
+app.engine('hbs', hadlebars.engine({
+    extname : 'hbs'
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
   })
 
-  app.listen(5000, ()=> console.log('Server is listening on port 5000...'))
+app.listen(5000, ()=> console.log('Server is listening on port 5000...'))
