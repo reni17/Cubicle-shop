faac8c94beb7d7b389fac6d84effb9319e94879a
reni17 <renit00@mail.bg>
2022-05-29 13:20:07 +0300

    render create-cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,5 @@
 const router = require('express'). Router()
-router.get('/', (req, res)=> {
-    res.render('index')
+router.get('/create', (req, res)=> {
+    res.render('create')
 })
 module.exports = router
--- src/views/routes.js
+++ src/views/routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const homeController = require('../controllers/homeController')
+const cubeController = require('../controllers/cubeController')
+
 
 const router = express.Router()
-router.get('/', homeController.index)
+router.use('/', homeController)
+router.use('/cube', cubeController)
+
 
 module.exports = router
