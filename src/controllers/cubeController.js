39e6df2bdb44648848051d0a6c083acf47099537
reni17 <renit00@mail.bg>
2022-05-31 13:11:19 +0300

    Add persistenced new rubic

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,13 +1,22 @@
 const router = require('express'). Router()
+const cubes = require('../cubes.json')
+const fs = require('fs/promises')
+const path = require('path')
 
 router.get('/create', (req, res)=> {
     res.render('create')
 })
 router.post('/create', (req, res)=> {
-    const cube =req.body
+    const cube = req.body
     //validate
 
     //save data
+ cubes.push(cube)
+ fs.writeFile(path.resolve('src', 'cubes.json'), JSON.stringify(cubes, '', 4), 'utf-8')
+ .then(()=> {
+     res.redirect('/')
+ })
+ .catch((err)=>alert(err))
 
     //redirect page
   })
