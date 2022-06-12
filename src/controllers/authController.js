95fd058b5a5f6e66f9ed3bbe86f9906c0d375372
reni17 <renit00@mail.bg>
2022-06-12 22:04:24 +0300

    Create jwt on successful login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,4 +19,20 @@ router.post('/register',async (req, res) => {
 
 })
 
+router.get('/login', (req, res)=> {
+
+    res.render('login')
+})
+
+router.post('/login',async (req, res) => {
+const token =await authService.login(req.body)
+
+if(!token){
+    res.redirect('/404')
+}
+console.log(token);
+res.redirect('/')
+
+})
+
 module.exports = router
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,10 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const saltRounds = 12
+const secret = 'heroeuhrfeofurhwejdsn'
+
 exports.register =async ({username, password, repeatPassword}) => {
   
 if(password !== repeatPassword){
@@ -15,4 +19,30 @@ let createdUser = User.create({
 })
 
 return createdUser
+}
+
+
+exports.login = async ({username, password}) => {
+  let user =await User.findOne({username})
+
+  if(!user){
+    return false
+  }
+
+  const isValid =await bcrypt.compare(password, user.password)
+  if(!isValid){
+    return 
+  }
+  let result = new Promise((resolve, reject) => {
+  jwt.sign({_id: user._id, username: user.username}, secret, {expiresIn: '2d'}, (err, token)=> {
+    if(err){
+        return reject(err)
+    }
+     resolve(token)
+  })
+
+  })
+  
+
+  return result
 }
